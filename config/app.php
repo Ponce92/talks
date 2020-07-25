bce8d3b5ee4d43191844eb2c6681f5e1daf7104c
Ponce92 <ponce92ues@outlok.com>
2020-07-24 21:28:02 -0600

    Despligue de sistema v0.02

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Talksamericas'),
+    'name' => env('APP_NAME', 'Payroll'),
 
     /*
     |--------------------------------------------------------------------------
